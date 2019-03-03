184f1ef5d7969e297dfb377d2eaadb3ba056a5e7
Vlad Feninets <v.feninets@gmail.com>
2019-03-03 12:56:50 +0200

    add env and debug page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 const PORT = 3000
 const API_PRODUCTS_URL = process.env.API_PRODUCTS_URL
 const API_BASKET_URL = process.env.API_BASKET_URL
@@ -26,6 +27,11 @@ app.get('/error', (req, res) => res.render('error'))
 app.get('/order/:id', (req, res) => res.render('order'))
 
 app.get('/cart/:id', (req, res) => res.render('cart'))
+
+app.get('/debug', (req, res) => {
+    axios.get(API_PAYMENT_URL).then(console.log)
+    res.send('')
+})
 
 app.get('**', (req, res) => res.render('404'))
 
