8e35ea45aba1d520fecc71b77086f5fd3659b250
Vlad Feninets <v.feninets@gmail.com>
2019-03-03 12:05:43 +0200

    add all needed pages

--- html-gateway/server.js
+++ html-gateway/server.js
@@ -4,6 +4,26 @@ const port = 3000
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', (req, res) => {
+    const products = [
+        { name: 'foo', price: 100, id: 'XYZ', photo: 'https://images.unsplash.com/photo-1551334741-0f11da38e980?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' },
+        { name: 'bar', price: 100, id: 'ZYZ', photo: 'https://images.unsplash.com/photo-1551334741-0f11da38e980?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' }
+    ]
+
+    res.render('home', { products })
+})
+
+app.get('/product/:id', (req, res) => {
+    const product = { name: 'foo', price: 100, descr: 'lorem ipsum dolorem', id: 'XYZ', photo: 'https://images.unsplash.com/photo-1551334741-0f11da38e980?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60' }
+    res.render('product', { product })
+})
+
+app.get('/error', (req, res) => res.render('error'))
+
+app.get('/order/:id', (req, res) => res.render('order'))
+
+app.get('/cart/:id', (req, res) => res.render('cart'))
+
+app.get('**', (req, res) => res.render('404'))
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
