bdf8a64dad50b36a72c676201697bdcbc8d1fe07
Vlad Feninets <v.feninets@gmail.com>
2019-03-03 11:18:43 +0200

    init

---
+++ html-gateway/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('home'))
+
+app.listen(port, () => console.log(`app listening on port ${port}!`))
